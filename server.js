49943f9bb3a8ea39720d57f48d5df12cc6f4f5b1
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-21 18:13:13 -1000

    created all header partials

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
   res.render('index');
 });

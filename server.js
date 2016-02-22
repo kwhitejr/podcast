0ff1d78d789005d90072bde3449e112f1a13968b
kwhitejr <kwhitejr.edu@gmail.com>
2016-02-21 17:35:39 -1000

    index.jade renders

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express();
 
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
 var server = app.listen(3000, function() {
   console.log('Listening to port', server.address().port);
 });
